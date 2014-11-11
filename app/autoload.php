076e36177442d938cf254f611443712b4e69d4b8
Pekin <pekin_jan@mail.ru>
2014-11-11 19:37:18 +0200

    gdsfgdfg

--- app/autoload.php
+++ app/autoload.php
@@ -3,11 +3,6 @@
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
 
-/**
- * @var ClassLoader $loader
- */
-$loader = require __DIR__.'/../vendor/autoload.php';
-
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 return $loader;
